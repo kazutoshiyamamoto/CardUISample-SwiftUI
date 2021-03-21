75a4ddefcac91c77827d2d407defc97b15edf0a6
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-03-21 15:14:16 +0900

    カード型のViewに必要なタイトルとURLの文字列を扱う変数を追加

--- CardUISample-SwiftUI/CardUISample-SwiftUI/CardView.swift
+++ CardUISample-SwiftUI/CardUISample-SwiftUI/CardView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct CardView: View {
+    var title: String
+    var urlString: String
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
