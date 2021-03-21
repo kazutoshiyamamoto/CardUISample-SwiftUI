336f66af3abb907bcc51f563460610c113dd75b8
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-03-21 13:53:23 +0900

    初回コミット

---
+++ CardUISample-SwiftUI/CardUISample-SwiftUI/CardUISample_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardUISample_SwiftUIApp.swift
+//  CardUISample-SwiftUI
+//
+//  Created by home on 2021/03/21.
+//
+
+import SwiftUI
+
+@main
+struct CardUISample_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
