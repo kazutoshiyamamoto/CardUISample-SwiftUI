726a40ca3e6b9e0601be38e736da324a31ebd5e3
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-03-21 15:13:16 +0900

    カード型のViewを実装するためのファイルを追加

---
+++ CardUISample-SwiftUI/CardUISample-SwiftUI/CardView.swift
@@ -0,0 +1,20 @@
+//
+//  CardView.swift
+//  CardUISample-SwiftUI
+//
+//  Created by home on 2021/03/21.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
