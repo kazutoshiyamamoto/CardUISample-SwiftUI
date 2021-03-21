f7067801b1c39e22142fb6ea839294ae3494dd87
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-03-21 16:18:39 +0900

    サンプルデータを修正

--- CardUISample-SwiftUI/CardUISample-SwiftUI/FavoritesData.swift
+++ CardUISample-SwiftUI/CardUISample-SwiftUI/FavoritesData.swift
@@ -19,11 +19,7 @@ var favorites: [Favorite] = [
     Favorite(id: 3, title: "Youtube", urlString: "https://www.youtube.com/"),
     Favorite(id: 4, title: "Amazon", urlString: "https://www.amazon.co.jp/"),
     Favorite(id: 5, title: "Twitter", urlString: "https://twitter.com/home"),
-    Favorite(id: 6, title: "Facebook", urlString: "https://www.facebook.com/"),
-    Favorite(id: 7, title: "Yahoo! JAPAN", urlString: "https://www.yahoo.co.jp/"),
-    Favorite(id: 8, title: "Yahoo! JAPAN", urlString: "https://www.yahoo.co.jp/"),
-    Favorite(id: 9, title: "Yahoo! JAPAN", urlString: "https://www.yahoo.co.jp/"),
-    Favorite(id: 10, title: "Yahoo! JAPAN", urlString: "https://www.yahoo.co.jp/"),
+    Favorite(id: 6, title: "Facebook", urlString: "https://www.facebook.com/")
 ]
 
 
